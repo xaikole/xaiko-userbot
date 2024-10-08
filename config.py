67023793febfadeb496673320baf88a1b413b567
Xaikole <ozosole4@gmail.com>
2024-10-08 11:47:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from distutils.util import strtobool as sb
 from base64 import b64decode
 
-
 load_dotenv()
 
 DEVS = [
@@ -54,31 +53,27 @@ GCAST_BLACKLIST = [
 
 class Config(object):
     # Telegram App KEY and HASH
-    API_KEY = int(getenv("API_KEY") or 0)
-    API_HASH = str(getenv("API_HASH") or None)
+    API_KEY = int(getenv("API_KEY", "0"))
+    API_HASH = getenv("API_HASH")
 
     # Inline bot helper
-    BOT_TOKEN = getenv("BOT_TOKEN", None)
-    BOT_USERNAME = getenv("BOT_USERNAME", None)
+    BOT_TOKEN = getenv("BOT_TOKEN")
+    BOT_USERNAME = getenv("BOT_USERNAME")
 
-    OPENAI_API_KEY = getenv("OPENAI_API_KEY", None)
+    OPENAI_API_KEY = getenv("OPENAI_API_KEY")
 
+    # SUDO and Blacklist settings
     SUDO_USERS = {int(x) for x in getenv("SUDO_USERS", "").split()}
     BL_CHAT = {int(x) for x in getenv("BL_CHAT", "").split()}
-    BLACKLIST_GCAST = {
-        int(x) for x in getenv(
-            "BLACKLIST_GCAST",
-            "").split()}
+    BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 
-    # For Blacklist Group Support
-    BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
-    if not BLACKLIST_CHAT:
-        BLACKLIST_CHAT = [-1001473548283, -1001675396283]
+    # Default Blacklist Chats
+    BLACKLIST_CHAT = [int(x) for x in (getenv("BLACKLIST_CHAT", "-1001473548283,-1001675396283")).split(",")]
 
     # Userbot Session String
-    STRING_SESSION = getenv("STRING_SESSION", None)
+    STRING_SESSION = getenv("STRING_SESSION")
 
-    # Logging channel/group ID configuration.
+    # Logging channel/group ID configuration
     BOTLOG_CHATID = int(getenv("BOTLOG_CHATID", "0"))
 
     # Load or No Load modules
@@ -87,76 +82,69 @@ class Config(object):
 
     # Bleep Blop, this is a bot ;)
     PM_AUTO_BAN = sb(getenv("PM_AUTO_BAN", "True"))
-    PM_LIMIT = int(getenv("PM_LIMIT", 6))
+    PM_LIMIT = int(getenv("PM_LIMIT", "6"))
 
     # Custom Handler command
-    CMD_HANDLER = getenv("CMD_HANDLER") or "."
+    CMD_HANDLER = getenv("CMD_HANDLER", ".")
     SUDO_HANDLER = getenv("SUDO_HANDLER", r"$")
 
     # Support
     GROUP = getenv("GROUP", "BestieVirtual")
     CHANNEL = getenv("CHANNEL", "Nenen_degrees")
 
-    # Heroku Credentials for updater.
-    HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
-    HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+    # Heroku Credentials for updater
+    HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+    HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
     # JustWatch Country
     WATCH_COUNTRY = getenv("WATCH_COUNTRY", "ID")
 
-    # Github Credentials for updater and Gitupload.
-    GIT_REPO_NAME = getenv("GIT_REPO_NAME", None)
-    GITHUB_ACCESS_TOKEN = getenv("GITHUB_ACCESS_TOKEN", None)
+    # GitHub Credentials for updater and Gitupload
+    GIT_REPO_NAME = getenv("GIT_REPO_NAME")
+    GITHUB_ACCESS_TOKEN = getenv("GITHUB_ACCESS_TOKEN")
 
-    # Custom (forked) repo URL for updater.
+    # Custom (forked) repo URL for updater
     UPSTREAM_REPO_URL = getenv("UPSTREAM_REPO_URL", "https://github.com/xaikole/xaiko-userbot.git")
 
     # Custom Name Sticker Pack
-    S_PACK_NAME = getenv("S_PACK_NAME", None)
+    S_PACK_NAME = getenv("S_PACK_NAME")
 
     # SQL Database URI
-    DB_URI = getenv("DATABASE_URL", None)
+    DB_URI = getenv("DATABASE_URL")
     DATABASE_PATH = os.path.join("ayiin.db")
 
     # OCR API key
-    OCR_SPACE_API_KEY = getenv("OCR_SPACE_API_KEY", None)
+    OCR_SPACE_API_KEY = getenv("OCR_SPACE_API_KEY")
 
     # remove.bg API key
     REM_BG_API_KEY = getenv("REM_BG_API_KEY", "jK9nGhjQPtd2Y5RhwMwB5EMA")
 
     # Chrome Driver and Headless Google Chrome Binaries
-    CHROME_DRIVER = getenv("CHROME_DRIVER") or "/usr/bin/chromedriver"
-    GOOGLE_CHROME_BIN = getenv(
-        "GOOGLE_CHROME_BIN") or "/usr/bin/google-chrome"
+    CHROME_DRIVER = getenv("CHROME_DRIVER", "/usr/bin/chromedriver")
+    GOOGLE_CHROME_BIN = getenv("GOOGLE_CHROME_BIN", "/usr/bin/google-chrome")
 
     # OpenWeatherMap API Key
-    OPEN_WEATHER_MAP_APPID = getenv("OPEN_WEATHER_MAP_APPID", None)
+    OPEN_WEATHER_MAP_APPID = getenv("OPEN_WEATHER_MAP_APPID")
     WEATHER_DEFCITY = getenv("WEATHER_DEFCITY", "Jakarta")
 
     # Anti Spambot Config
     ANTI_SPAMBOT = sb(getenv("ANTI_SPAMBOT", "False"))
     ANTI_SPAMBOT_SHOUT = sb(getenv("ANTI_SPAMBOT_SHOUT", "False"))
 
-    # untuk perintah teks costum .alive
-    ALIVE_TEKS_CUSTOM = getenv(
-        "ALIVE_TEKS_CUSTOM",
-        "Hey, Saya pengguna Fsyrl-Ubot")
+    # Custom text for .alive command
+    ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, Saya pengguna Fsyrl-Ubot")
 
     # Default .alive name
     ALIVE_NAME = getenv("ALIVE_NAME", "Fsyrl")
 
-    # Custom Emoji Alive
+    # Custom Emojis for .alive command
     ALIVE_EMOJI = getenv("ALIVE_EMOJI", "✧")
-
-    # Custom Emoji Alive
     INLINE_EMOJI = getenv("INLINE_EMOJI", "✵")
-
-    # Custom icon HELP
     ICON_HELP = getenv("ICON_HELP", "⍟")
 
     # Time & Date - Country and Time Zone
-    COUNTRY = str(getenv("COUNTRY", "ID"))
-    TZ_NUMBER = int(getenv("TZ_NUMBER", 1))
+    COUNTRY = getenv("COUNTRY", "ID")
+    TZ_NUMBER = int(getenv("TZ_NUMBER", "1"))
 
     # Clean Welcome
     CLEAN_WELCOME = sb(getenv("CLEAN_WELCOME", "True"))
@@ -164,43 +152,38 @@ class Config(object):
     # Zipfile module
     ZIP_DOWNLOAD_DIRECTORY = getenv("ZIP_DOWNLOAD_DIRECTORY", "./zips")
 
-    # bit.ly module
-    BITLY_TOKEN = getenv("BITLY_TOKEN", None)
+    # Bitly token
+    BITLY_TOKEN = getenv("BITLY_TOKEN")
 
     # Bot version
     BOT_VER = getenv("BOT_VER", "4.0.0")
 
     # Default .alive logo
-    ALIVE_LOGO = (getenv("ALIVE_LOGO")
-                or "https://telegra.ph//file/b49aa34c29b05fe22d9c7.jpg")
+    ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph//file/b49aa34c29b05fe22d9c7.jpg")
 
-    INLINE_PIC = (getenv("INLINE_PIC")
-                or "https://telegra.ph//file/07fb28c1efb9058543241.jpg")
+    # Inline picture
+    INLINE_PIC = getenv("INLINE_PIC", "https://telegra.ph//file/07fb28c1efb9058543241.jpg")
 
     # Picture For VCPLUGIN
-    PLAY_PIC = (getenv("PLAY_PIC")
-                or "https://telegra.ph/file/6213d2673486beca02967.png")
+    PLAY_PIC = getenv("PLAY_PIC", "https://telegra.ph/file/6213d2673486beca02967.png")
+    QUEUE_PIC = getenv("QUEUE_PIC", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 
-    QUEUE_PIC = (getenv("QUEUE_PIC")
-                or "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
-
+    # Default values
     DEFAULT = list(map(int, b64decode("MTkwNTA1MDkwMw==").split()))
 
-    TEMP_DOWNLOAD_DIRECTORY = getenv(
-        "TMP_DOWNLOAD_DIRECTORY", "./downloads/")
+    TEMP_DOWNLOAD_DIRECTORY = getenv("TMP_DOWNLOAD_DIRECTORY", "./downloads/")
 
     # Deezloader
-    DEEZER_ARL_TOKEN = getenv("DEEZER_ARL_TOKEN", None)
+    DEEZER_ARL_TOKEN = getenv("DEEZER_ARL_TOKEN")
 
     # NSFW Detect DEEP AI
-    DEEP_AI = getenv("DEEP_AI", None)
+    DEEP_AI = getenv("DEEP_AI")
 
-    # Tambahkan CMD_HELP
+    # Help command
     CMD_HELP = {
         "start": "Memulai bot",
         "help": "Menampilkan daftar perintah",
         # Tambahkan perintah lain sesuai kebutuhan
     }
-
 
 var = Config()
